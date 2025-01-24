e579d2ffa4c30983a3d315980b6e47051775d0bf
nadilaarfntiii <nadila4313@gmail.com>
2025-01-24 14:04:23 +0700

    update

--- app/Views/layouts/components/topbar.php
+++ app/Views/layouts/components/topbar.php
@@ -43,8 +43,7 @@
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span class="mr-2 d-none d-lg-inline text-gray-600 medium" style="margin-right: 8px;">Admin</span>
-                    <img class="img-profile rounded-circle"
-                        src="../assets/img/foto.png">
+                    <img class="img-profile rounded-circle" src="<?= base_url('assets/img/foto.png'); ?>">
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
